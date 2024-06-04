011562a4625088c97eb686c34b1247dd5d88e137
ghufron akbar <alan.040219.gam@gmail.com>
2024-06-04 17:37:24 +0700

    Middleware

---
+++ middleware/verifikasi-admin.js
@@ -0,0 +1,34 @@
+const jwt = require("jsonwebtoken");
+const config = require("../config/secret");
+
+function verifikasiAdmin(req, res, next) {
+  // Ambil token dari header Authorization
+  let token = req.headers["authorization"];  
+  
+  if (token) {
+    // Remove 'Bearer ' jika ada
+    token = token.replace(/^Bearer\s+/, "");
+    
+    // Verifikasi token
+    jwt.verify(token, config.secret, function (err, decoded) {
+      if (err) {
+        return res.status(401).send({ auth: false, message: "Token tidak terdaftar!" });
+      } else {
+        // Cek waktu kadaluarsa token
+        const currentTime = Math.floor(Date.now() / 1000); // Waktu saat ini dalam detik
+        if (decoded.exp && decoded.exp < currentTime) {
+          return res.status(401).send({ auth: false, message: "Session habis!" });
+        } if (! decoded.rows[0].id_admin){
+          return res.status(401).send({ auth: false, message: "Anda bukan admin" });
+        }
+        
+        req.decoded = decoded; // Menyimpan data decoded ke dalam req untuk penggunaan selanjutnya
+        next(); // Lanjutkan ke middleware/route selanjutnya
+      }
+    });
+  } else {
+    return res.status(401).send({ auth: false, message: "Token tidak tersedia!" });
+  }
+}
+
+module.exports = verifikasiAdmin;
--- routes/admin.js
+++ routes/admin.js
@@ -1,105 +1,100 @@
 "use strict";
 
-// const verifikasi = require("./middleware/verifikasi");
+const verifikasiAdmin = require('../middleware/verifikasi-admin');
+const api_admin = require("../controllers/admin");  
 
 module.exports = function (app) {
-  let api_admin = require("../controllers/admin");
-  // asd
-  //LOGIN
 
+  //LOGIN
   app.route(`/api/admin/login`)
     .post(api_admin.account_controller.login);
 
   //MENU CONTROLLER
-
   app.route(`/api/admin/products`)
-    .get(api_admin.product_controller.products);
+    .get(verifikasiAdmin,  api_admin.product_controller.products);
 
   app.route(`/api/admin/product/:id_product`)
-    .get(api_admin.product_controller.productId);
+    .get(verifikasiAdmin,  api_admin.product_controller.productId);
 
   app.route(`/api/admin/product/add`)
-    .post(api_admin.product_controller.productAdd);
+    .post(verifikasiAdmin,  api_admin.product_controller.productAdd);
 
   app.route(`/api/admin/product/edit/:id_product`)
-    .put(api_admin.product_controller.productEdit);
+    .put(verifikasiAdmin,  api_admin.product_controller.productEdit);
 
   app.route(`/api/admin/product/setstock/:id_product`)
-    .put(api_admin.product_controller.productSetStock);
+    .put(verifikasiAdmin,  api_admin.product_controller.productSetStock);
 
   app.route(`/api/admin/product/delete/:id_product`)
-    .delete(api_admin.product_controller.productDelete);
+    .delete(verifikasiAdmin,  api_admin.product_controller.productDelete);
 
 
   // ORDER CONTROLLER
-
   app.route(`/api/admin/orders`)
-    .get(api_admin.order_controller.allOrder);
+    .get(verifikasiAdmin,  api_admin.order_controller.allOrder);
 
   app.route(`/api/admin/order/:id_history`)
-    .get(api_admin.order_controller.orderId);
+    .get(verifikasiAdmin,  api_admin.order_controller.orderId);
 
   app.route(`/api/admin/orders/0`)
-    .get(api_admin.order_controller.allOrderPending);
+    .get(verifikasiAdmin,  api_admin.order_controller.allOrderPending);
 
   app.route(`/api/admin/orders/1`)
-    .get(api_admin.order_controller.allOrderCancelByUser);
+    .get(verifikasiAdmin,  api_admin.order_controller.allOrderCancelByUser);
 
   app.route(`/api/admin/orders/2`)
-    .get(api_admin.order_controller.allOrderCancelByAdmin);
+    .get(verifikasiAdmin,  api_admin.order_controller.allOrderCancelByAdmin);
 
   app.route(`/api/admin/orders/3`)
-    .get(api_admin.order_controller.allOrderPaid);
+    .get(verifikasiAdmin,  api_admin.order_controller.allOrderPaid);
 
   app.route(`/api/admin/orders/4`)
-    .get(api_admin.order_controller.allorderprocess);
+    .get(verifikasiAdmin,  api_admin.order_controller.allorderprocess);
 
   app.route(`/api/admin/orders/5`)
-    .get(api_admin.order_controller.allOrderReady);
+    .get(verifikasiAdmin,  api_admin.order_controller.allOrderReady);
 
   app.route(`/api/admin/orders/6`)
-    .get(api_admin.order_controller.allOrderDone);
+    .get(verifikasiAdmin,  api_admin.order_controller.allOrderDone);
 
 
   //HANDLE CONTROLLER
   app.route(`/api/admin/order/cancel/:id_history`)
-    .put(api_admin.handle_controller.handleCancelByAdmin);
+    .put(verifikasiAdmin,  api_admin.handle_controller.handleCancelByAdmin);
 
   app.route(`/api/admin/order/process/:id_history`)
-    .put(api_admin.handle_controller.handleProcess);
+    .put(verifikasiAdmin,  api_admin.handle_controller.handleProcess);
 
   app.route(`/api/admin/order/ready/:id_history`)
-    .put(api_admin.handle_controller.handleReady);
+    .put(verifikasiAdmin,  api_admin.handle_controller.handleReady);
 
   app.route(`/api/admin/order/done/:id_history`)
-    .put(api_admin.handle_controller.handleDone);
-
-
+    .put(verifikasiAdmin,  api_admin.handle_controller.handleDone);
 
 
   //STATISTIC CONTROLLER
   app.route(`/api/admin/order/total/done`)
-    .get(api_admin.statistic_controller.totalDone);
+    .get(verifikasiAdmin,  api_admin.statistic_controller.totalDone);
 
   app.route(`/api/admin/order/total/income/today`)
-    .get(api_admin.statistic_controller.totalIncomeToday);
+    .get(verifikasiAdmin,  api_admin.statistic_controller.totalIncomeToday);
 
   app.route(`/api/admin/order/total/income/month`)
-    .get(api_admin.statistic_controller.totalIncomeMonth);
+    .get(verifikasiAdmin,  api_admin.statistic_controller.totalIncomeMonth);
 
   app.route(`/api/admin/order/total/income/monthly`)
-    .get(api_admin.statistic_controller.totalIncomeMonthly);
+    .get(verifikasiAdmin,  api_admin.statistic_controller.totalIncomeMonthly);
 
   app.route(`/api/admin/order/total/income/daily`)
-    .get(api_admin.statistic_controller.totalIncomeDaily);
+    .get(verifikasiAdmin,  api_admin.statistic_controller.totalIncomeDaily);
 
 
   // INFORMATION CONTROLLER
   app.route(`/api/admin/information`)
-    .get(api_admin.information_controller.infoPayment);
+    .get(verifikasiAdmin,  api_admin.information_controller.infoPayment);
 
   app.route(`/api/admin/information/edit`)
-    .put(api_admin.information_controller.editPaymnet);
+    .put(verifikasiAdmin,  api_admin.information_controller.editPaymnet);
 
 };
 
